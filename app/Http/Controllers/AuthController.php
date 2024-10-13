0eb50ea74726e10912a3796e3a25deed65eb8a19
Flank Silva <flank.silva@cenariocapital.com.br>
2024-10-13 13:53:46 -0300

    create validate login page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,29 @@ class AuthController extends Controller
        return view('login');
     }
 
+    public function loginSubmit (Request $request) {
+        // form validation
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.required' => 'Usuario obrigatório',
+                'text_username.email' => 'Formato de email inválido',
+                'text_password.required' => 'Senha obrigatória',
+                'text_password.min' => 'Senha deve ter pelo menos 6 caracteres',
+                'text_password.max' => 'Senha deve ter no máximo 16 caracteres'
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
+    }
+
     public function logout() {
         echo "logout";
     }
